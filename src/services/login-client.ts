65c2f4ac515f3848548911df80c7090f9b25b801
ImMacii <66750429+ImMaci@users.noreply.github.com>
2024-06-19 09:11:19 +0200

    /api added to requests

--- src/services/login-client.ts
+++ src/services/login-client.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 export const registerClient = axios.create({
-    baseURL: "http://localhost:8080/auth/register"
+    baseURL: "http://localhost:8080/api/auth/register"
 });
 
 export const loginClient = axios.create({
-    baseURL: "http://localhost:8080/auth/authenticate"
+    baseURL: "http://localhost:8080/api/auth/authenticate"
 });
